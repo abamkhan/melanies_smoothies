2eea974fac9e7ffbd45bcf79b96156daee0ae1ee
abamkhan <abdullah.amer.khan@gmail.com>
2025-09-16 14:22:06 +0000

    Dynamic Fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen +' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
+        st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
 
     st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -59,7 +63,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         #st.text(smoothiefroot_response.json(), use_container_width=True)
-        st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+        
         st.success('Your Smoothie is ordered!', icon="✅")
         
 
